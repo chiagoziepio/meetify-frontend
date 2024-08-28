7394707a24e8f44e361d144d3f2d2848f9f1d1d2
christopherpio <nduaguba.christopher@standardlife.org.ng>
2024-08-28 09:45:49 +0100

    package setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='text-2xl'>meetify</div>
+      
+    </>
+  )
+}
+
+export default App
